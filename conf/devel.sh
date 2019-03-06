0cdab17d9a47e2b0657a1b89921534f04f9f634e
chenwx <chenwx716@163.com>
2019-03-07 06:21:12 +0800

    update docker bash script

--- conf/devel.sh
+++ conf/devel.sh
@@ -2,8 +2,94 @@
 
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp $work_dir/devel.yaml $work_dir/../alarm/conf.yaml
-cp $work_dir/devel.yaml $work_dir/../bserver/conf.yaml
-cp $work_dir/devel.yaml $work_dir/../gserver/conf.yaml
-cp $work_dir/devel.yaml $work_dir/../mserver/conf.yaml
-cp $work_dir/devel.yaml $work_dir/../moniter/conf.yaml
+#===================================================================
+
+conf_file=$work_dir/devel.yaml
+
+cp $conf_file $work_dir/../alarm/conf.yaml
+cp $conf_file $work_dir/../bserver/conf.yaml
+cp $conf_file $work_dir/../gserver/conf.yaml
+cp $conf_file $work_dir/../mserver/conf.yaml
+cp $conf_file $work_dir/../moniter/conf.yaml
+
+cd ..
+#===================================================================
+
+version=0.1
+
+#------------------------------------------------
+echo "=========================================="
+echo "del run log: $work_dir"
+echo "=========================================="
+find $work_dir -name '*.log' -exec rm {} \;
+#------------------------------------------------
+# bserver
+container=bserver
+
+echo "=========================================="
+echo "build: $container"
+echo "=========================================="
+
+docker stop $container
+docker rm $container
+docker rmi chenwx/$container:$version
+
+docker build -t chenwx/$container:$version -f dockerfile/$container.dockerfile .
+
+#------------------------------------------------
+# gserver
+container=gserver
+
+echo "=========================================="
+echo "build: $container"
+echo "=========================================="
+
+docker stop $container
+docker rm $container
+docker rmi chenwx/$container:$version
+
+docker build -t chenwx/$container:$version -f dockerfile/$container.dockerfile .
+
+#------------------------------------------------
+# mserver
+container=mserver
+
+echo "=========================================="
+echo "build: $container"
+echo "=========================================="
+
+docker stop $container
+docker rm $container
+docker rmi chenwx/$container:$version
+
+docker build -t chenwx/$container:$version -f dockerfile/$container.dockerfile .
+
+#------------------------------------------------
+# moniter
+container=moniter
+
+echo "=========================================="
+echo "build: $container"
+echo "=========================================="
+
+docker stop $container
+docker rm $container
+docker rmi chenwx/$container:$version
+
+docker build -t chenwx/$container:$version -f dockerfile/$container.dockerfile .
+
+#------------------------------------------------
+# alarm
+container=alarm
+
+echo "=========================================="
+echo "build: $container"
+echo "=========================================="
+
+docker stop $container
+docker rm $container
+docker rmi chenwx/$container:$version
+
+docker build -t chenwx/$container:$version -f dockerfile/$container.dockerfile .
+
+#===================================================================
